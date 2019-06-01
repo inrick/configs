6548baf595e408f29dcc4aee52b35cd1428a669e
Fabian Carlström <inrick@gmail.com>
2019-06-01 10:54:47 +0200

    Firefox: add user.js

---
+++ misc/user.js
@@ -0,0 +1,8 @@
+// Firefox user.js
+//
+// See also prefs.js
+user_pref("browser.library.activity-stream.enabled", false);
+user_pref("extensions.pocket.enabled", false);
+user_pref("identity.fxaccounts.enabled", false);
+user_pref("layers.acceleration.force-enabled", true);
+user_pref("security.webauth.u2f", true);
