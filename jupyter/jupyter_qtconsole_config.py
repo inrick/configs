7e618391b423bc61826e6ee110d24954fe72fdac
Fabian Carlström <inrick@gmail.com>
2021-10-10 09:22:23 +0200

    qtconsole: font update

--- jupyter/jupyter_qtconsole_config.py
+++ jupyter/jupyter_qtconsole_config.py
@@ -7,8 +7,8 @@ c.JupyterWidget.editor = os.environ['EDITOR']
 c.ConsoleWidget.buffer_size = 10000
 c.ConsoleWidget.console_height = 26
 c.ConsoleWidget.console_width = 84
-c.ConsoleWidget.font_family = 'Inconsolata Medium'
-c.ConsoleWidget.font_size = 13
+c.ConsoleWidget.font_family = 'Source Code Pro Medium'
+c.ConsoleWidget.font_size = 11
 c.ConsoleWidget.gui_completion = 'ncurses'
 
 c.JupyterQtConsoleApp.stylesheet = os.path.join(os.environ['HOME'], '.jupyter', 'qtconsole.css')
