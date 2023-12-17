2e2c07b4d9b0a214ec42c634d1c59686ffa3dc84
Fabian Carlström <inrick@gmail.com>
2023-12-17 13:40:42 +0100

    Add comment

--- config/plasma-workspace/env/myenv.sh
+++ config/plasma-workspace/env/myenv.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+# NOTE: The SDDM Xsession will also source ~/.zprofile when one's shell is zsh,
+# see /usr/share/sddm/scripts/Xsession.
 export SSH_ASKPASS='/usr/bin/ksshaskpass'
 export GTK_USE_PORTAL=1
