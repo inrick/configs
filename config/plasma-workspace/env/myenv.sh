c5acf466af388d0c10acce7b625e07add72c4cf5
Fabian Carlström <inrick@gmail.com>
2024-08-12 10:23:49 +0200

    KDE changes

--- config/plasma-workspace/env/myenv.sh
+++ config/plasma-workspace/env/myenv.sh
@@ -2,4 +2,7 @@
 # NOTE: The SDDM Xsession will also source ~/.zprofile when one's shell is zsh,
 # see /usr/share/sddm/scripts/Xsession.
 export SSH_ASKPASS='/usr/bin/ksshaskpass'
-export GTK_USE_PORTAL=1
+
+# Use 1 to force KDE open file dialogs, for instance. But it makes rendering
+# terrible when using Wayland, in which case 0 is preferred.
+export GTK_USE_PORTAL=0
