6ea464c4b5500b14768a4caed4e099c68dcce20b
Fabian Carlström <inrick@gmail.com>
2025-09-27 11:52:49 +0200

    Firefox

--- misc/user.js
+++ misc/user.js
@@ -6,3 +6,8 @@ user_pref("extensions.pocket.enabled", false);
 user_pref("identity.fxaccounts.enabled", false);
 user_pref("layers.acceleration.force-enabled", true);
 user_pref("security.webauth.u2f", true);
+
+// Scrollbar size
+user_pref("widget.non-native-theme.scrollbar.style", 4);
+// Compact mode
+user_pref("ui.density", 1);
